07864dd9e4af876b1a229980dfc20de16e5e87da
RainlyTree <a1123845350@163.com>
2020-05-06 18:08:03 +0800

    tm类年限要 + 1900

--- date_time/date_time_test.cpp
+++ date_time/date_time_test.cpp
@@ -19,14 +19,29 @@ int main()
     date d7 = from_undelimited_string("20011118");
 
     //显示当前日期
+    cout << "显示当前日期:" << endl;
     cout << day_clock::local_day() << endl;
     cout << day_clock::universal_day() << endl;
 
 
     //日期输出
+    cout << "日期输出:" << endl;
     cout << to_simple_string(d2) << endl;
     cout << to_iso_string(d2) << endl;
     cout << to_iso_extended_string(d2) << endl;
     cout << d2 << endl;
+
+    //与tm转换
+    cout << "tm 与 date转换:" << endl;
+    tm t = to_tm(d6);
+    cout << "转换成tm 时的时间: ";
+    //转换之后年份有问题
+    cout << t.tm_year + 1900 << " " << t.tm_mon << " " << t.tm_mday << 
+        " " << t.tm_hour << " " << t.tm_min << " " << t.tm_sec << endl;
+
+    cout << "转换成date 时间:";
+    date d8 = date_from_tm(t);
+    cout << d8 << endl;
+
     return 0;
 }
