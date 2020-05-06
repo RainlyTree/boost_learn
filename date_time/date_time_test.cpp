02b73996edce01f252b0afbd36dc5c31d67506a2
RainlyTree <a1123845350@163.com>
2020-05-06 20:29:06 +0800

    data_period

--- date_time/date_time_test.cpp
+++ date_time/date_time_test.cpp
@@ -1,3 +1,8 @@
+
+#undef BOOST_DATE_TIME_OPTIONAL_GREGORIAN_TYPES
+//使用这个宏定义 则不包含greg_duration_types.hpp这个头文件
+//防止 weeks months years时长类被无意使用
+
 #include <boost/date_time/gregorian/gregorian.hpp>  //处理日期的组件
 using namespace boost::gregorian;
 
@@ -7,8 +12,12 @@ using namespace boost::posix_time;
 #include<iostream>
 using namespace std;
 
+
 int main()
 {
+    months m(3);
+    cout << m.number_of_months();
+    weeks w(3);
     date d1;      //无效的日期
     date d2(2010, 1, 1);  //数字构造日期
     date d3(2020, Jan, 1);  //英文指定月份
@@ -50,5 +59,8 @@ int main()
     d4 += months(1);
     cout << "year : month : day :" << d4.year() << " " << d4.month() << " " << d4.day() << endl;
 
+    //date_period类  日期区间
+    date_period dp1(date(2017, 1, 1), days(20));
+    cout << "date_period: " << dp1 << endl;
     return 0;
 }
